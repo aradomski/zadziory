92f21458cdf83ce7ef6c22e774445f523603d6e0
Dorian Sawa <dorian.revan@gmail.com>
2012-01-05 10:41:44 +0100

    galeria zdjeci, imageflow + highslide w jquery

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def flash_display
+  response = ""
+  flash.each do |name, msg|
+    response = response + content_tag(:div, msg, :id => "flash_#{name}")
+  end
+  flash.discard
+  response
+end
+
 end
