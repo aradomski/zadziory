3cf02f64192d546038db2e0aafe9f2fad59b796a
Dorian Sawa <dorian.revan@gmail.com>
2012-01-06 17:40:47 +0100

    provider dla twittera

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '274845445906273', '0b7da014298093bb66e010c0b12d1060'
+  provider :twitter, '7tpvkdTWgeeSrN4oOXQw', 'wznMtnX8qo5iD5jhKlDZpVvxoJcGb0LviJZXKxlmo'
 end
