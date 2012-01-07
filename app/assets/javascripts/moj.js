a40a02557113fa601f530f8aa29c1a95dcd0dbd5
Adam <niewpisze@gmail.com>
2012-01-07 21:52:59 +0100

    http://www.youtube.com/watch?v=DE7JLpDkzc8

--- app/assets/javascripts/moj.js
+++ app/assets/javascripts/moj.js
@@ -1,14 +1,33 @@
  $(function(){
         $('.descripton span').hide();
         $('.descripton h3').click(function(){
-            if ($(this).text() == 'Rozwin opis') {
+            if ($(this).text() == '+ Rozwin opis') {
 				//alert("rozwijam");
-                $(this).text('Zwin opis');
+                $(this).text('- Zwin opis');
                 $('.descripton span').slideDown('slow');
             } else {
 				//alert("zwijam");
-                $(this).text('Rozwin opis');
+                $(this).text('+ Rozwin opis');
                 $('.descripton span').slideUp('slow');
             }
         });
     });
+$(function(){
+	box = $('.grade_stars');
+	ocena = box.text();
+	box.text('');
+	//alert (ocena);
+	var insert= '';
+	for (i=0;i<ocena;i++){
+		insert = insert + '<img alt="true" height="15" src="/assets/star.png" width="15"/>';
+	}
+	box.html(insert);
+});
+$(function(){
+	var aval = $('.show_header input:hidden');
+	//alert(aval.attr('value'));
+	if(aval.attr('value') == "true"){
+		aval.parent().css('background', '#1eff00');
+	}
+
+});
