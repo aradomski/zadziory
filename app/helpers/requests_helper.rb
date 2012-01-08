9878af6f702400bad8bf1645a8680ba36c52f38b
pslaski <pslaski@inf.ug.edu.pl>
2012-01-08 21:27:03 +0100

    poprawka selecta wyboru statusu

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -21,8 +21,20 @@ module RequestsHelper
     User.find(id).email.to_s
   end
 
-  def get_status_collection
-    {get_status(1) => 1, get_status(2) => 2, get_status(3) => 3, get_status(0) => 0}
+
+  # options => :all  return all options
+  # options => :pending return options for pending status (accepted/rejected)
+  # options => :accepted return options for accepted status (rented/rejected)
+  def get_status_collection(options)
+    case options
+      when :all
+        {get_status(1) => 1, get_status(2) => 2, get_status(3) => 3, get_status(0) => 0}
+      when :pending
+        {get_status(2) => 2, get_status(0) => 0}
+      when :accepted
+        {get_status(3) => 3, get_status(0) => 0}
+    end
+
   end
 
 end
