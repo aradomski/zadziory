c09ea975bfdf94395e92f10f689ebf05705c6348
Marzena <mmarciniak@sigma.ug.edu.pl>
2012-01-09 02:14:39 +0100

    pl

--- db/schema.rb
+++ db/schema.rb
@@ -164,5 +164,6 @@ ActiveRecord::Schema.define(:version => 20120108222146) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
